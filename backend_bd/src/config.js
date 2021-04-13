1b4b2192040658684b261ca67d881e53738030c3
IgorBaio <igorbaiosoares@gmail.com>
2021-04-13 18:23:01 -0300

    Separação de repositorios

--- backend_bd/src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    connectionString: 'mongodb+srv://admin:admin@cluster0.gvnfs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    sendgridKey: 'TBD',
-    containerConnectionString: 'TDB',
-}
